f251027caa0298de0ebd875891a0dfd513f9356f
Jonathan Beliën <dev@jbelien.be>
2019-06-10 09:11:19 +0200

    Update ListTypes.jsx
    Make elections and elections types configurable.

--- src/components/ListTypes.jsx
+++ src/components/ListTypes.jsx
@@ -1,45 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { elections, electionsTypes } from "../config";
 
 export default class extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.year = parseInt(props.match.params.year);
+    this.election = elections.find(e => e.year === this.year);
+  }
+
+  renderList() {
+    return this.election.types.map(type => {
+      return (
+        <li key={type}>
+          <Link to={`/${this.election.year}/${type}`}>
+            {electionsTypes[type].fr}
+            <br />
+            {electionsTypes[type].nl}
+          </Link>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>{this.props.match.params.year}</h1>
-        <ul>
-          <li key="EU">
-            <Link to={`/${this.props.match.params.year}/EU`}>
-              Parlement européen / Europees Parlement
-            </Link>
-          </li>
-          <li key="CK">
-            <Link to={`/${this.props.match.params.year}/CK`}>
-              Chambre / Kamer
-            </Link>
-          </li>
-          <li key="BR">
-            <Link to={`/${this.props.match.params.year}/BR`}>
-              Parlement de la Région de Bruxelles-Capitale / Brussels
-              Hoofdstedelijk Parlement
-            </Link>
-          </li>
-          <li key="VL">
-            <Link to={`/${this.props.match.params.year}/VL`}>
-              Parlement flamand / Vlaams Parlement
-            </Link>
-          </li>
-          <li key="WL">
-            <Link to={`/${this.props.match.params.year}/WL`}>
-              Parlement régional wallon / Waals Parlement
-            </Link>
-          </li>
-          <li key="DE">
-            <Link to={`/${this.props.match.params.year}/DE`}>
-              Parlement de la Communauté germanophone / Parlement van de
-              Duitstalige Gemeenschap
-            </Link>
-          </li>
-        </ul>
+        <h1>{this.election.year}</h1>
+
+        <ul>{this.renderList()}</ul>
       </div>
     );
   }
---
+++ src/config.js
@@ -0,0 +1,50 @@
+export const elections = [
+  {
+    year: 2019,
+    types: ["CK", "BR", "VL", "WL", "DE", "EU"],
+    previous: 2014
+  }
+];
+
+export const electionsTypes = {
+  BR: {
+    fr: "Parlement de la Région de Bruxelles-Capitale",
+    nl: "Brussels Hoofdstedelijk Parlement"
+  },
+  CG: {
+    fr: "Conseil communal",
+    nl: "Gemeenteraden"
+  },
+  CK: {
+    fr: "Chambre",
+    nl: "Kamer"
+  },
+  CS: {
+    fr: "Conseil CPAS",
+    nl: "OCMWraden"
+  },
+  DE: {
+    fr: "Parlement de la Communauté germanophone",
+    nl: "Parlement van de Duitstalige Gemeenschap"
+  },
+  DI: {
+    fr: "Conseil de district (Anvers)",
+    nl: "Districtraden (in Antwerpen)"
+  },
+  EU: {
+    fr: "Parlement européen",
+    nl: "Europees Parlement"
+  },
+  PR: {
+    fr: "Conseil provincial",
+    nl: "Provincieraden"
+  },
+  VL: {
+    fr: "Parlement flamand",
+    nl: "Vlaams Parlement"
+  },
+  WL: {
+    fr: "Parlement régional wallon ",
+    nl: "Waals Parlement"
+  }
+};
