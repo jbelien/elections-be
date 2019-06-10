871472a3a97d79b89a52d5cb6b979b14b08467a4
Jonathan Beliën <dev@jbelien.be>
2019-06-10 21:03:31 +0200

    Add votes and seats charts

--- src/components/ListGroups.jsx
+++ src/components/ListGroups.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import qs from "query-string";
+import { Bar as BarChart, Doughnut as DoughnutChart } from "react-chartjs-2";
 
-import { electionsTypes } from "../config";
+import { api, electionsTypes } from "../config";
 
 import "../assets/sass/listGroups.scss";
 
@@ -15,7 +16,9 @@ export default class extends React.Component {
     this.state = {
       filter: typeof filter !== "undefined" ? parseInt(filter) : null,
       groups: [],
-      entities: {}
+      entities: {},
+      result: {},
+      results: []
     };
   }
 
@@ -23,21 +26,21 @@ export default class extends React.Component {
     const { year, type } = this.props.match.params;
 
     Promise.all([
+      fetch(`${api}/groups/${year}/${type}`).then(response => response.json()),
+      fetch(`${api}/lists/${year}/${type}`).then(response => response.json()),
+      fetch(`${api}/entities/${year}/${type}`).then(response =>
+        response.json()
+      ),
       fetch(
-        `https://api.elections.openknowledge.be/v1/groups/${year}/${type}`
-      ).then(response => response.json()),
-      fetch(
-        `https://api.elections.openknowledge.be/v1/lists/${year}/${type}`
-      ).then(response => response.json()),
-      fetch(
-        `https://api.elections.openknowledge.be/v1/entities/${year}/${type}`
+        `${api}/format-r/result/${year}/${type}/${type !== "DE" ? "R" : "G"}`
       ).then(response => response.json())
     ]).then(data => {
       const groups = data[0];
       const lists = data[1];
       const entities = data[2];
+      const result = data[3];
 
-      let result = Object.values(groups).map(group => {
+      let groupsWithLists = Object.values(groups).map(group => {
         group.lists = Object.values(lists).filter(list => {
           if (this.state.filter !== null) {
             return (
@@ -50,11 +53,31 @@ export default class extends React.Component {
         return group;
       });
 
-      result = result.filter(group => group.lists.length > 0);
+      groupsWithLists = groupsWithLists.filter(group => group.lists.length > 0);
+
+      let results = [];
+      result.lists.forEach(list => {
+        results.push({
+          group: groups[list.idGroup],
+          seats: list.seats,
+          votes:
+            list.countSubCategory1 +
+            list.countSubCategory2 +
+            list.countSubCategory3 +
+            list.countSubCategory4,
+          total:
+            result.count.countBBE1E2 +
+            result.count.countE3E4 +
+            result.count.countE5 -
+            (result.count.countBlankBBE1E2E5 + result.count.countBlankE3E4)
+        });
+      });
 
       this.setState({
-        groups: result,
-        entities: entities
+        groups: groupsWithLists,
+        entities: entities,
+        result: result,
+        results: results
       });
     });
   }
@@ -91,7 +114,7 @@ export default class extends React.Component {
   renderList(list, group) {
     return (
       <li
-      className="list-groups-lists-list"
+        className="list-groups-lists-list"
         key={list.id}
         style={{ borderColor: `#${group.color}` }}
       >
@@ -110,6 +133,9 @@ export default class extends React.Component {
   render() {
     const { year, type } = this.props.match.params;
 
+    const votesChart = this.votesChart();
+    const seatsChart = this.seatsChart();
+
     return (
       <div>
         <h1>{year}</h1>
@@ -118,6 +144,22 @@ export default class extends React.Component {
           <br />
           {electionsTypes[type].nl}
         </h2>
+        <div className="list-groups-charts">
+          <div>
+            <BarChart
+              data={votesChart.data}
+              options={votesChart.options}
+              height={150}
+            />
+          </div>
+          <div>
+            <DoughnutChart
+              data={seatsChart.data}
+              options={seatsChart.options}
+              height={150}
+            />
+          </div>
+        </div>
         <ul className="list-groups">
           {this.state.groups
             .sort((a, b) => (a.nr || Infinity) - (b.nr || Infinity))
@@ -125,5 +167,105 @@ export default class extends React.Component {
         </ul>
       </div>
     );
+  }
+
+  votesChart() {
+    const { results } = this.state;
+
+    let labels = [];
+    let colors = [];
+    let data = [];
+    results
+      // .filter(group => group.votes > 0)
+      .sort((a, b) => b.votes - a.votes)
+      .forEach(group => {
+        labels.push(group.group.name);
+        colors.push(`#${group.group.color}`);
+        data.push(Math.round((group.votes / group.total) * 100 * 100) / 100);
+      });
+
+    return {
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            data: data,
+            backgroundColor: colors,
+            borderColor: colors,
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: {
+          xAxes: [
+            {
+              ticks: {
+                display: false
+              }
+            }
+          ],
+          yAxes: [
+            {
+              ticks: {
+                display: false
+              }
+            }
+          ]
+        },
+        title: {
+          display: true,
+          text: "Votes (%)"
+        }
+      }
+    };
+  }
+
+  seatsChart() {
+    const { results } = this.state;
+
+    let labels = [];
+    let colors = [];
+    let data = [];
+    results
+      .filter(group => group.seats > 0)
+      .sort((a, b) => b.seats - a.seats)
+      .forEach(group => {
+        labels.push(group.group.name);
+        colors.push(`#${group.group.color}`);
+        data.push(group.seats);
+      });
+
+    return {
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            data: data,
+            backgroundColor: colors,
+            borderColor: colors,
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        circumference: Math.PI,
+        legend: {
+          display: false
+        },
+        maintainAspectRatio: false,
+        responsive: true,
+        rotation: -Math.PI,
+        title: {
+          display: true,
+          text: "Seats"
+        }
+      }
+    };
   }
 }
